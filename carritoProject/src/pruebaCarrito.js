0e87c4688c913160648601f82c864e4992acee52
Istner <desarrollo@istner.com>
2021-10-13 16:58:22 +0200

    interactive pruebaCarrito

--- carritoProject/src/pruebaCarrito.js
+++ carritoProject/src/pruebaCarrito.js
@@ -1,25 +1,55 @@
-const http = require('http');
-const fs = require("fs").promises;
+var readline = require('readline');
+const internal = require('stream');
 const { Carrito } = require("./carrito");
 
-// crea un objeto para acceder al carrito
-// se debe de inicializar después
-carro = new Carrito("Fran");
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
 
-async function run(carro){
-    await carro.cogerCarrito();
-    carro.toString().then(console.log);
-    //console.log(carroStr);
-    await carro.add("manzanas", 2);
-    await carro.add("manzanas", 3);
-    try{
-        await carro.remove("manzanas", 3);
-        await carro.remove("platanos", 3); // salta excepcion
-    } catch (err) {
-        console.error(err.stack);
+const home_msg = "--------BIENVENIDO AL PORTAL DE COMPRA ONLINE--------\n[1] AÑADIR PRODUCTO\t[2] ELIMINAR PRODUCTO\t[3] MOSTRAR CARRO"
+const carro = new Carrito("Fran");
+
+let initialized = false;
+let modifyMod = false;
+let addMod = false;
+let deleteMod = false;
+
+const inicio = async function(line){
+    if(!initialized){
+        initialized = true;
+        await carro.cogerCarrito();
     }
-    await carro.add("cereza", 2);
+    if(!modifyMod) {
+        if(line==="1") {
+            console.log("INTRODUZCA EL PRODUCTO Y LA CANTIDAD A AÑADIR SEPARADOS POR UN ESPACIO (ej. Manzanas 2)");
+            modifyMod = true;
+            addMod = true;
+        } else if(line ==="2") {
+            console.log("INTRODUZCA EL PRODUCTO Y LA CANTIDAD A ELIMINAR SEPARADOS POR UN ESPACIO (ej. Manzanas 2)"); 
+            modifyMod = true;
+            deleteMod = true;
+        } else if(line==="3") {
+            let contenidos = await carro.toString();
+            console.log(contenidos["productos"]);
+        }
+    } else {
+        let [prod, cant] = line.split(" ");
+        if (prod != null && cant != null) {
+            if (deleteMod) {
+                deleteMod  = false;
+                carro.remove(prod.toLowerCase(), cant).catch(console.log);
+            } else if (addMod) {
+                addMod = false;
+                carro.add(prod.toLowerCase(), cant);
+            }
+        }
+        modifyMod = false;
+    }
+    if (!modifyMod) console.log(home_msg);
+};
 
-    carro.toString().then(console.log);
-}
-run(carro);
+console.log(home_msg);
+
+rl.on('line', inicio);
