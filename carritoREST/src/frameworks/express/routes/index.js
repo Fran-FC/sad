131fc9eb930cb649fc452a6738b6b6775bf3d01f
Fran-FC <frafolcm@gmail.com>
2021-10-29 23:25:20 +0200

    created structure for carritoREST

--- carritoREST/src/frameworks/express/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userRoutes = require('./users')
-
-module.exports = dependencies =>{
-    const routes = express.Router();
-    const users = userRoutes(dependencies);
-    routes.use('/users',users)
-    return routes;
-}
