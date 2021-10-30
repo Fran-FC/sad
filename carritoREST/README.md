0ecfbd27def4ae11139b5c584100ccf088e458de
Fran-FC <frafolcm@gmail.com>
2021-10-30 15:15:28 +0200

    added persistency to the service registry

--- carritoREST/README.md
+++ carritoREST/README.md
@@ -7,3 +7,12 @@ Este ejercicio se ha realizado y desplegado en una única máquina, por lo que c
 En la figura se muestra el esquema de los servicios una vez los dos (Carro y MongoDB Client) se han registrado en el Service Registry. Posteriormente, cuando se quiera acceder por primera vez a uno de los servicios, se deberá preguntar al service registry cual es la url necesaria para comunicarse con este. Una vez se haya obtenido, ya no hará falta volver a preguntarle.
 
 <img src="imgs/diagramaCarroREST1.png">
+
+## Service Registry
+
+El Service Registy es el proveedor de la "ubicación" de cada servicio a quien lo recueste, y guardará la url de los servicios que quieran ofrecerse al mundo. Por lo tanto, será el primer componente en ponerse en marcha. 
+
+La implementación de este Service Registry que se nos proporciona es algo limitada, ya que no tiene una persistencia fuerte, esto quiere decir que si por algún motivo el componente "muere", perderá todas las entradas de los servicios previamente guardados, y estos servicios deberán registrarse de nuevo. Como propuesta alternativa, se puede implementar un fichero json donde se guarden los servicios, y que sea independiente a la ejecución del programa, de esta manera, los cambios sucederían de manera permanente.
+
+Para ofrecer siempre servicios "vivos", cada servicio tendrá un timeout de 10 segundos por defecto, una vez pasado el timeout, se eliminará el servicio. Esto va a requerir que cada componente registre su servicio cada 10 segundos (por lo menos) si quiere ser accesible en todo momento. Por otro lado, si un cliente quiere acceder a un servicio y obtiene una respuesta 404 puede volver a intentarlo en un tiempo o cancelar la opercación.
+
--- carritoREST/service-registry/server/lib/ServiceRegistry.js
+++ carritoREST/service-registry/server/lib/ServiceRegistry.js
@@ -1,22 +1,33 @@
 const semver = require('semver');
+const fs = require("fs");
+const path = require("path");
+
+
+const servicesJsonPath = path.join(__dirname, '..', 'services.json');
+console.log(servicesJsonPath);
+
 
 class ServiceRegistry {
   constructor(log) {
     this.log = log;
     this.services = {};
-    this.timeout = 10;
+    this.timeout = 20;
   }
 
   get(name, version) {
-    this.cleanup();
+    this.#getServicesFromFile();
+    console.log(this.services);
+    this.#cleanup();
     const candidates = Object.values(this.services)
       //.filter(service => service.name === name && semver.satisfies(service.version, ">="+semver.clean(version)));
       .filter(service => service.name === name && service.version === version); // for this simple case, only check if version equals 
+    this.#setServicesToFile();
     return candidates[Math.floor(Math.random() * candidates.length)];
   }
 
   register(name, version, ip, port) {
-    this.cleanup();
+    this.#getServicesFromFile();
+    this.#cleanup();
     const key = name + version + ip + port;
 
     if (!this.services[key]) {
@@ -27,20 +38,25 @@ class ServiceRegistry {
       this.services[key].name = name;
       this.services[key].version = version;
       this.log.debug(`Added services ${name}, version ${version} at ${ip}:${port}`);
+      this.#setServicesToFile();
       return key;
     }
     this.services[key].timestamp = Math.floor(new Date() / 1000);
+    this.#setServicesToFile();
     this.log.debug(`Updated services ${name}, version ${version} at ${ip}:${port}`);
     return key;
   }
 
   unregister(name, version, ip, port) {
+    this.#getServicesFromFile();
     const key = name + version + ip + port;
     delete this.services[key];
+    this.#setServicesToFile();
     return key;
   }
 
-  cleanup() {
+  #cleanup() {
+    this.#getServicesFromFile();
     const now = Math.floor(new Date() / 1000);
     Object.keys(this.services).forEach((key) => {
       if (this.services[key].timestamp + this.timeout < now) {
@@ -48,6 +64,24 @@ class ServiceRegistry {
         this.log.debug(`Removed service ${key}`);
       }
     });
+    this.#setServicesToFile();
+  }
+  
+  #getServicesFromFile() {
+    try{
+      this.services = JSON.parse(fs.readFileSync(servicesJsonPath));
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
+  #setServicesToFile(){
+    try {
+      fs.writeFileSync(servicesJsonPath, JSON.stringify(this.services));
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } 
   }
 }
 
